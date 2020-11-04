f4d3c7139e12b99a78e4f76c866786331c460f2d
matteo_lovato <lovato.teo@gmail.com>
2020-11-04 16:45:20 +0100

    add same effects with gsap scrolltrigger

--- app.js
+++ app.js
@@ -70,3 +70,29 @@
 
 // GSAP SCROLL TRIGGER
 
+const slides = document.querySelectorAll(".slide");
+const nav = document.querySelector(".nav-header");
+
+slides.forEach((slide) => {
+  const img = slide.querySelector("img");
+  const revealImg = slide.querySelector(".reveal-img");
+  const revealText = slide.querySelector(".reveal-text");
+
+  const tl = new gsap.timeline({
+    defaults: {
+      duration: 1,
+      ease: "power2.inOut",
+    },
+    scrollTrigger: {
+      trigger: slide,
+      start: "top center",
+      markers: true,
+      toggleAction: "play none none reverse",
+    },
+  });
+
+  tl.fromTo(img, { scale: 2 }, { scale: 1 })
+    .fromTo(revealImg, { x: "0%" }, { x: "100%" }, "-=1")
+    .fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75")
+    .fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+});
