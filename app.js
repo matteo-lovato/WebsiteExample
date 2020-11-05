86c03ed15451a802c077086b4d70976b6a03b652
matteo_lovato <lovato.teo@gmail.com>
2020-11-05 15:16:11 +0100

    add on explore button mouse hover title color will be filled in

--- app.js
+++ app.js
@@ -132,8 +132,10 @@ function activeCursor(e) {
   if (item.classList.contains("explore")) {
     mouse.classList.add("explore-active");
     mouseText.innerText = "Tap";
+    gsap.to(".title-swipe", 1, { y: "0%" });
   } else {
     mouse.classList.remove("explore-active");
+    gsap.to(".title-swipe", 1, { y: "100%" });
     mouseText.innerText = "";
   }
 }
